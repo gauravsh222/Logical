b5b8ab48e05ffcbf94c0ff6d4fcbc9288b3bea49
gauravsh222 <gauravsh222@gmail.com>
2018-05-09 16:55:39 +0530

    Update CHM.java

--- Practice/src/com/threads/CHM.java
+++ Practice/src/com/threads/CHM.java
@@ -1,5 +1,6 @@
 package com.threads;
 
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
@@ -16,51 +17,65 @@ class CHMRead implements Runnable {
 	@Override
 	public void run() {
 		Set<Entry<String, Integer>> entrySet = map.entrySet();
-		try { System.out.println("Read Sleeping"); Thread.sleep(2000); } catch
-		 (InterruptedException e) { e.printStackTrace(); }
-		/*Iterator<Entry<String, Integer>> it = entrySet.iterator();
-		while (it.hasNext()) {
-			System.out.println("Found Element");
+		Iterator<Entry<String, Integer>> it = entrySet.iterator();
+		
+		try {
+			System.out.println("Read Sleeping");
+			Thread.sleep(5000); 
+		} catch (InterruptedException e) { 
+			e.printStackTrace(); 
+		}
+		
+		System.out.println("read Wokeup");
+		boolean b;
+		while (b=it.hasNext()) {
+			System.out.println(b);
 			Entry<String, Integer> next = it.next();
 			System.out.println("Read : " + next.getKey() + " : " + next.getValue());
-		}*/
-		
-		for(Entry<String, Integer> entry : entrySet) {
-			System.out.println(entry.getKey() + " : " + entry.getValue());
 		}
+		
+		/*for(Entry<String, Integer> entry : entrySet) {
+			System.out.println(entry.getKey() + " : " + entry.getValue());
+		}*/
 	}
 }
 
 class CHMWrite implements Runnable {
-
 	 Map<String, Integer> map;
-
 	public CHMWrite(Map<String, Integer> map) {
 		this.map = map;
 	}
-
 	@Override
 	public void run() {
-		/*try {
+		try {
 			System.out.println("Write Sleeping");
-			Thread.sleep(100);
+			Thread.sleep(500);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
-		}*/
-		map.put("A", 2);
-		System.out.println("Write : " + map.get("A"));
-		map.remove("A");
-		System.out.println("Write : " + map.get("A"));
+		}
+		System.out.println("Write Wokeup");
+		map.put("ABC", 2);
+		map.put("XYZ", 2);
+		System.out.println("Write A : " + map.get("XYZ"));
+		System.out.println("Write B : " + map.get("ABC"));
+		map.remove("ABC");
+		map.remove("XYZ");
+		System.out.println("Write A : " + map.get("XYZ"));
+		System.out.println("Write B : " + map.get("ABC"));
+		map.put("ABC", 5);
+		map.put("XYZ", 5);
+		System.out.println("Write A : " + map.get("XYZ"));
+		System.out.println("Write B : " + map.get("ABC"));
 	}
-
 }
 
 public class CHM {
-	volatile Map<String, Integer> map ;
+	Map<String, Integer> map ;
 	public static void main(String[] args) {
 		CHM c = new CHM();
 		c.map = new ConcurrentHashMap<String, Integer>();
-		c.map.put("A", 1);
+		c.map.put("ABC", 1);
+		c.map.put("XYZ", 1);
 		Thread t1 = new Thread(new CHMRead(c.map), "Read");
 		Thread t2 = new Thread(new CHMWrite(c.map), "Write");
 		t1.start();
